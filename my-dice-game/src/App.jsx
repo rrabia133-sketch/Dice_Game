09961b124aa38ddd427db4104997a185c7414cb8
rrabia133-sketch <rrabia133@gmail.com>
2025-09-17 12:38:31 +0800

    button

--- my-dice-game/src/App.jsx
+++ my-dice-game/src/App.jsx
@@ -1,7 +1,15 @@
-import "./App.css";
-
+import styled from "styled-components";
+const button = styled.button`
+  background-color: black;
+  color: white;
+  padding: 10px;
+`;
 function App() {
-  return <></>;
+  return (
+    <>
+      <button>This is styled component button</button>
+    </>
+  );
 }
 
 export default App;
